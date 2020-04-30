5c5e84d61367b3b85cce25106f3600387e3fae5a
Amol Sontakke <amols@proximabiz.com>
2020-04-30 17:05:30 +0530

    [#1006] Completed dashboard UI implementation
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

---
+++ src/app/modules/dashboard/component/dashboard-contributor-buttons/dashboard-contributor-buttons.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-contributor-buttons',
+  templateUrl: './dashboard-contributor-buttons.component.html',
+  styleUrls: ['./dashboard-contributor-buttons.component.scss']
+})
+export class DashboardContributorButtonsComponent implements OnInit {
+  @Input() type: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
