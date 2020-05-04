108c784ab2e3f41dd153e072380a388d5f349497
Amol Sontakke <amols@proximabiz.com>
2020-05-04 17:31:44 +0530

    Removed lint error and test again
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- src/app/shared/svg/individual-icon/individual-icon.component.ts
+++ src/app/shared/svg/individual-icon/individual-icon.component.ts
@@ -1,19 +1,15 @@
 // Copyright The Linux Foundation and each contributor to CommunityBridge.
 // SPDX-License-Identifier: MIT
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-individual-icon',
   templateUrl: './individual-icon.component.html',
   styleUrls: ['./individual-icon.component.scss']
 })
-export class IndividualIconComponent implements OnInit {
+export class IndividualIconComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-    const error = '';
-  }
 
 }
