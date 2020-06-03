a4c9aef4124645b50da499fae101d2512a0ca475
Amol Sontakke <amols@proximabiz.com>
2020-06-03 21:22:57 +0530

    Added spec.ts to shared components
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

---
+++ src/app/shared/components/project-title/project-title.component.spec.ts
@@ -0,0 +1,30 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ProjectTitleComponent } from './project-title.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AlertService } from '../../services/alert.service';
+import { StorageService } from '../../services/storage.service';
+import { HttpClientModule } from '@angular/common/http';
+
+describe('ProjectTitleComponent', () => {
+    let component: ProjectTitleComponent;
+    let fixture: ComponentFixture<ProjectTitleComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ProjectTitleComponent],
+            imports: [RouterTestingModule,HttpClientModule],
+            providers: [AlertService, StorageService]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ProjectTitleComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
