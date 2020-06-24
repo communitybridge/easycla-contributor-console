a77bd84d0bebff2ddd88cad075972ab366c63da0
Amol Sontakke <amols@proximabiz.com>
2020-06-24 13:11:58 +0530

    [#1016] Changed UI as per new theme Updated add company regex as per organization service
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- src/app/config/app-settings.ts
+++ src/app/config/app-settings.ts
@@ -12,6 +12,6 @@ export class AppSettings {
     public static HEADER_CONTENT_TYPE = 'application/json';
     public static EMAIL_PATTERN = '[a-z|A-Z|0-9]+[@]+[a-z|A-Z|0-9]+[.]+([a-z|A-Z|0-9]){2}';
     public static USERNAME_REGEX = '/^[a-zA-Z0-9_]{1,15}$/';
-    public static COMPANY_NAME_REGEX = '^[a-zA-Z0-9- ]*$';
+    public static COMPANY_NAME_REGEX = '^([\\w\\d\\s\\-\\,\\./]+)$';
     public static URL_PATTERN = '^((ht|f)tp(s?))\://([0-9a-zA-Z\-]+\.)+[0-9a-zA-Z]{2,6}(\:[0-9]+)?(/\S*)?$';
 }
