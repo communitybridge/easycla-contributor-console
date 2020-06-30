aaa21c43b0be042e1abf3c3cdb5f7e6ace623e22
Amol Sontakke <56335654+amolsontakke3576@users.noreply.github.com>
2020-06-30 10:56:39 +0530

    Change checkbox text on individual contributor screen (#49)

--- src/app/modules/individual-contributor/component/docusign-signature-model/docusign-signature-model.component.ts
+++ src/app/modules/individual-contributor/component/docusign-signature-model/docusign-signature-model.component.ts
@@ -1,28 +1,19 @@
 // Copyright The Linux Foundation and each contributor to CommunityBridge.
 // SPDX-License-Identifier: MIT
 
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-docusign-signature-model',
   templateUrl: './docusign-signature-model.component.html',
   styleUrls: ['./docusign-signature-model.component.scss']
 })
-export class DocusignSignatureModelComponent implements OnInit {
+export class DocusignSignatureModelComponent {
   @Input() status: string;
   @Output() backBtnEmitter: EventEmitter<any> = new EventEmitter<any>();
   @Output() signCLAEmitter: EventEmitter<any> = new EventEmitter<any>();
-  checkedItems: string[];
 
   constructor() { }
-
-  ngOnInit(): void {
-    this.checkedItems = [
-      'Generating Contract',
-      'Adding Information',
-      'Document Ready'
-    ];
-  }
 
   onClickBack() {
     this.backBtnEmitter.emit();
