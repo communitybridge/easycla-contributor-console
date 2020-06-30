e00ea7052eb1cbf6cd9f75cd277ddac6437ca269
Amol Sontakke <amols@proximabiz.com>
2020-06-30 09:30:09 +0530

    [1326] Changed text on checkbox in ICLA screen
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- src/app/modules/individual-contributor/component/docusign-signature-model/docusign-signature-model.component.ts
+++ src/app/modules/individual-contributor/component/docusign-signature-model/docusign-signature-model.component.ts
@@ -8,21 +8,12 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
   templateUrl: './docusign-signature-model.component.html',
   styleUrls: ['./docusign-signature-model.component.scss']
 })
-export class DocusignSignatureModelComponent implements OnInit {
+export class DocusignSignatureModelComponent {
   @Input() status: string;
   @Output() backBtnEmitter: EventEmitter<any> = new EventEmitter<any>();
   @Output() signCLAEmitter: EventEmitter<any> = new EventEmitter<any>();
-  checkedItems: string[];
 
   constructor() { }
-
-  ngOnInit(): void {
-    this.checkedItems = [
-      'Generating Contract',
-      'Adding Information',
-      'Document Ready'
-    ];
-  }
 
   onClickBack() {
     this.backBtnEmitter.emit();
