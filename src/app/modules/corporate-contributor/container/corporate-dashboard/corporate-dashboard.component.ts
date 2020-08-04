7f162e70f24694fa7cb1b566afb6a9bf09409b8e
Amol Sontakke <amols@proximabiz.com>
2020-08-04 19:12:00 +0530

    [#1130] added encodeURIComponent for search oraganization parameter
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- src/app/modules/corporate-contributor/container/corporate-dashboard/corporate-dashboard.component.ts
+++ src/app/modules/corporate-contributor/container/corporate-dashboard/corporate-dashboard.component.ts
@@ -218,7 +218,7 @@ export class CorporateDashboardComponent {
         clearTimeout(this.searchTimeout);
       }
       this.searchTimeout = setTimeout(() => {
-        this.searchOrganization(value);
+        this.searchOrganization(encodeURIComponent(value));
       }, 300);
     } else {
       this.organizationList.list = [];
