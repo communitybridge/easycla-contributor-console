fb02ef6b32c69458780e6e3ac95eee3d6741965f
David Deal <dealako@gmail.com>
2020-10-05 17:58:54 -0700

    Infra Updates (#126)
    - Updated serverless version
    - Updated cloudfront invalidation logic - fixed distribution id
    - Removed unused environment in serverless - alleviated serverless schema warning
    - Used serverless.js rather than serverless - alleviated warning
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -12,5 +12,5 @@ export const environment = {
   LFX_HEADER_URL: 'https://cdn.dev.platform.linuxfoundation.org/lfx-header.js',
   ACCEPTABLE_USER_POLICY: 'https://communitybridge.dev.platform.linuxfoundation.org/acceptable-use/',
   SERVICE_SPECIFIC_TERM: 'https://communitybridge.dev.platform.linuxfoundation.org/service-terms/',
-  PLATEFORM_USER_AGREEMENT: 'https://communitybridge.dev.platform.linuxfoundation.org/platform-use-agreement/'
+  PLATFORM_USER_AGREEMENT: 'https://communitybridge.dev.platform.linuxfoundation.org/platform-use-agreement/'
 };
