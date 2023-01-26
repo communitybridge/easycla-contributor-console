3ec83b6ded13b22e6088415efea8364089db1bba
Amol Sontakke <amols@proximabiz.com>
2023-01-26 21:36:03 +0530

    Solved lint issue
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 // Copyright The Linux Foundation and each contributor to CommunityBridge.
 // SPDX-License-Identifier: MIT
 
-import { Component } from "@angular/core";
-import { AppSettings } from "./config/app-settings";
-import { LfxHeaderService } from "./shared/services/lfx-header.service";
-import { EnvConfig } from "./config/cla-env-utils";
-import { environment } from "src/environments/environment";
+import { Component } from '@angular/core';
+import { AppSettings } from './config/app-settings';
+import { LfxHeaderService } from './shared/services/lfx-header.service';
+import { EnvConfig } from './config/cla-env-utils';
+import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = "easycla-contributor-console";
+  title = 'easycla-contributor-console';
   hasExpanded: boolean;
   links: any[];
 
@@ -30,15 +30,15 @@ export class AppComponent {
   }
 
   private mountHeader(): void {
-    const script = document.createElement("script");
-    script.setAttribute("src", environment.lfxHeader + "/lfx-header-v2.js");
-    script.setAttribute("async", "true");
+    const script = document.createElement('script');
+    script.setAttribute('src', environment.lfxHeader + '/lfx-header-v2.js');
+    script.setAttribute('async', 'true');
     document.head.appendChild(script);
   }
 
   private mountFooter(): void {
-    const script = document.createElement("script");
-    script.setAttribute("src", EnvConfig.default[AppSettings.LFX_FOOTER]);
+    const script = document.createElement('script');
+    script.setAttribute('src', EnvConfig.default[AppSettings.LFX_FOOTER]);
     document.head.appendChild(script);
   }
 }
