ff38495073a0b4146ad6b118457d481b6353c23e
csavulalfx <cavula@contractor.linuxfoundation.org>
2024-03-04 16:46:05 +0530

    Add aws-sdk
    Signed-off-by: csavulalfx <cavula@contractor.linuxfoundation.org>

--- src/app/config/scripts/prefetch-ssm.mjs
+++ src/app/config/scripts/prefetch-ssm.mjs
@@ -2,7 +2,8 @@
 // SPDX-License-Identifier: MIT
 //const fs = require('fs');
 import fs from 'fs';
-const RetrieveSSMValues = require('./read-ssm');
+//const RetrieveSSMValues = require('./read-ssm');
+import RetrieveSSMValues from './read-ssm';
 const configVarArray = ['auth0-clientId', 'auth0-domain', 'proj-console-link', 'corp-console-link', 'lfx-header', 'lfx-footer', 'corporate-v2-base', 'api-base', 'api-v4-base', 'admin-v2-base'];
 const region = 'us-east-1';
 const profile = process.env.AWS_PROFILE;
